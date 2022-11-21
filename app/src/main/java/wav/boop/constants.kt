9f2c56cc683a263aa91002eaae40b195803ef8a5
Daniel Soskey <asks@danielsoskey.com>
2022-11-20 19:48:42 -0800

    removed most of view code. im keeping sampler code while i work on the sampler menu refresh

--- app/src/main/java/wav/boop/constants.kt
+++
@@ -1,4 +0,0 @@
-package wav.boop
-
-const val SPINNER_ITEM_ID = R.layout.boop_spinner_item
-const val SPINNER_DROPDOWN_ITEM_ID = R.layout.boop_spinner_dropdown_item
--- app/src/main/java/wav/boop/visualisation/constants.kt
+++
@@ -1,3 +0,0 @@
-package wav.boop.visualisation
-
-const val DEFAULT_SAMPLE_RATE_IN_SECONDS: Int = 44100
